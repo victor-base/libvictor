48cd9df8b970364a567c6c7e998013887756579a
Emiliano A. Billi <emiliano.billi@gmail.com>
2025-04-05 21:13:45 -0300

    license

--- src/method.h
+++ src/method.h
@@ -3,19 +3,20 @@
  * 
  * Copyright (C) 2025 Emiliano A. Billi
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This file is part of libvictor.
  *
- * This program is distributed in the hope that it will be useful,
+ * libvictor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * libvictor is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libvictor. If not, see <https://www.gnu.org/licenses/>.
  * Contact: emiliano.billi@gmail.com
  *
  * Purpose:
