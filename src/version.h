a28c5f07431e99c802f48f97404c3855f365ee78
Emiliano A. Billi <emiliano.billi@gmail.com>
2025-08-19 09:24:23 -0300

    update Makefile

---
+++ src/version.h
@@ -0,0 +1,16 @@
+#ifndef _VERSION_H
+#define _VERSION_H
+
+#define __LIB_VERSION_MAJOR "1"
+#define __LIB_VERSION_MINOR "0"
+#define __LIB_VERSION_PATCH "17"
+#define __LIB_VERSION_STRING "1.0.17"
+
+/* Build information */
+#define __BUILD_DATE "2025-08-17 23:31:45"
+#define __BUILD_HOST "MacBookPro.fibertel.com.ar"
+#define __BUILD_USER "emilianobilli"
+#define __BUILD_OS "Darwin"
+#define __BUILD_ARCH "arm64"
+
+#endif
